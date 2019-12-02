9827c2982526c077581d06c3f604bcd3089045b6
Wout van Riel <PS183072@summacollege.nl>
2019-12-02 13:53:52 +0100

    mailing

--- ProjectLJ3/resources/views/email/herinnering.blade.php
+++ ProjectLJ3/resources/views/email/herinnering.blade.php
@@ -6,7 +6,8 @@
         Geachte {{ $name }},      
         <br/>
         <br/>
-        Bij deze willen we U uitnodigen om een stageplek aan te vragen via de link hieronder:     
+        Wij sturen deze mail omdat we U willen herinneren dat U uitgenodigd bent om een stageplek aan te vragen bij het Summa College.
+        Deze aanvraag kunt U versturen via deze link: 
         <br/>
         <?php echo "<a href='http://localhost:8000/enquete?uuid=" . $uuid ."' >Klik hier om een stageverzoek in te dienen</a>";
         ?>
